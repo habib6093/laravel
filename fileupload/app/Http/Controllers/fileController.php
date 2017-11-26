1bd826e181817662795311b9094a76e957c1e53b
habib6093 <habibrahman6093@gmail.com>
2017-11-27 04:13:50 +0600

    added video uploading option

--- fileupload/app/Http/Controllers/fileController.php
+++ fileupload/app/Http/Controllers/fileController.php
@@ -4,18 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
 
 class fileController extends Controller
 {
     public function upload()
     {
+       // echo phpinfo();
     	return view('file.upload');
     }
 
     public function save(request $request)
     {
-    	if($request->hasFile('file'))
+
+
+     $rules=[
+          'file'=>'max:100000',
+
+     ];
+     $validator = Validator::make($request->all(),$rules);
+
+  //echo "string";
+        //$this->validate($request,['file'=>'required|mimes:jpeg,bmp,png|size:10000']);
+
+        
+
+    	//if($request->hasFile('file'))
+   if(!($validator->fails()))
     	{
+
+
 
 	    //	$m=$request->file('file')->store('public');
             $m=Storage::putFile('public',$request->file('file'));
@@ -24,9 +43,20 @@ class fileController extends Controller
 	        $n=Storage::url($m);
 
 	        echo "is: $n<br>";
-            
+
+            $name=$request->file('file')->getClientOriginalName();
+            echo "file name is: $name";
+
+            $type=$request->file('file')->getMimeType();
+            echo "type is: ".$type;
+
             //return Storage::allFiles('public');
-	    	return view('file.showFile',compact('n'));
+            if(strpos($type, 'video') !== false)
+            {
+                return view('file.showVideoFile',compact('n'));
+            }
+            else
+	    	  return view('file.showFile',compact('n')); //for image
         }
         else
         {
---
+++ fileupload/resources/views/file/showVideoFile.blade.php
@@ -0,0 +1,3 @@
+<video width="400" controls>
+  <source src="{{$n}}" type="video/mp4">
+</video>
